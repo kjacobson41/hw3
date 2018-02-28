999f1464976d34c057170ed76ae2664704eb13d8
Kevin Jacobson <kevin.jacobson@kellogg.northwestern.edu>
2018-02-28 17:01:00 -0600

    hw3

--- weather.js
+++ weather.js
@@ -1,19 +1,25 @@
+let geoCheck = function(event) {
+  navigator.geolocation.getCurrentPosition(getWeather);
+}
+
+jQuery('#get_forecast').on('click',geoCheck)
+
 let updateWidget = function(data) {
 
-  console.log("Got weather data: ", data)
-  // YOUR CODE GOES HERE
+  let tempNow = data.main.temp.toFixed(0)
+  let icon = data.weather["0"].icon
+  let city = data.name
 
-  // HINT:
-  // Weather icons are provided for you. Sample URL: http://openweathermap.org/img/w/01d.png
-  // The very last part ('01d.png') should be obtained from the weather information.
+  jQuery(".card-text").text("It is " + tempNow + " degrees outside.")
+  jQuery(".card-title").text(city)
+  jQuery("img").attr("src", "http://openweathermap.org/img/w/" + icon + ".png")
 
 }
 
-
-let getWeather = function(event) {
-  let latitude = '48.8566';
-  let longitude = '2.3522';
-  let apiKey = '4ce6f502d38ddae567bf1702b05e168c'; // REPLACE THIS VALUE with your own key.
+let getWeather = function(info) {
+  let latitude = info.coords.latitude.toFixed(4);
+  let longitude = info.coords.longitude.toFixed(4);
+  let apiKey = 'a73dc7790533e5fe3c02bf43bf5d65f5';
 
   let weatherServiceURL = 'https://api.openweathermap.org/data/2.5/weather?'
   weatherServiceURL += 'lat=' + latitude
@@ -23,10 +29,6 @@ let getWeather = function(event) {
   fetch(weatherServiceURL).then(convertToJSON).then(updateWidget).catch(displayError);
 }
 
-
-////////////////////////////////////////////////////////////
-// YOU DO NOT NEED TO CHANGE ANY CODE BEYOND THIS POINT
-////////////////////////////////////////////////////////////
 
 let convertToJSON = function(rawData) { return rawData.json(); }
 let displayError = function(error) { console.debug(error); }
